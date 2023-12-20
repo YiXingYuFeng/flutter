c3bb8be73ad33535b834c53a3a17a0159392ab60
You <lei.zhu@gaialab.cn>
2023-12-20 19:02:26 +0800

    feat: flutter

--- flutter_application_1/lib/login_page.dart
+++ flutter_application_1/lib/login_page.dart
@@ -58,10 +58,15 @@ class _LoginPageState extends State<LoginPage> {
           ),
           Container(
             width: duSetWidth(110),
+            margin: EdgeInsets.only(top: 10),
             child: Text(
               'GAIA',
+              textAlign: TextAlign.center,
               style: TextStyle(
                 color: AppColor.primaryText,
+                fontFamily: "Avenir",
+                fontWeight: FontWeight.w800,
+                fontSize: duSetFontSize(16),
               ),
             ),
           )
@@ -71,6 +76,88 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   // 登录表单
+  Widget _buildFrom() {
+    return Container(
+      width: duSetWidth(300),
+      margin: EdgeInsets.only(top: duSetHeight(80)),
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: AppColor.secondaryElement,
+                width: 2,
+              ),
+            ),
+            child: TextField(
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: '请输入账号',
+                contentPadding: EdgeInsets.fromLTRB(20, 10, 10, 10),
+              ),
+              style: TextStyle(
+                color: AppColor.primaryText,
+                fontFamily: "Avenir",
+                fontWeight: FontWeight.w400,
+                fontSize: duSetFontSize(18),
+              ),
+              maxLines: 1,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: const Color.fromARGB(51, 246, 246, 246),
+                width: 2,
+              ),
+            ),
+            child: TextField(
+              keyboardType: TextInputType.visiblePassword,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: '请输入账号密码',
+                contentPadding: EdgeInsets.fromLTRB(20, 10, 10, 10),
+              ),
+              style: TextStyle(
+                color: AppColor.primaryText,
+                fontFamily: "Avenir",
+                fontWeight: FontWeight.w400,
+                fontSize: duSetFontSize(18),
+              ),
+              maxLines: 1,
+            ),
+          ),
+          // 登录按钮
+          Container(
+            width: duSetWidth(295),
+            height: duSetHeight(44),
+            margin: EdgeInsets.only(top: 10),
+            decoration: BoxDecoration(
+              color: AppColor.secondaryElementText,
+            ),
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all(AppColor.secondaryElementText),
+              ),
+              child: Text(
+                '登录',
+                style: TextStyle(
+                  color: AppColor.primaryElementText,
+                  fontFamily: "Avenir",
+                  fontWeight: FontWeight.normal,
+                  fontSize: duSetFontSize(16),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
 
   // 第三方登录
 
@@ -83,6 +170,7 @@ class _LoginPageState extends State<LoginPage> {
         child: Column(
           children: [
             _buildLogo(),
+            _buildFrom(),
           ],
         ),
       ),
